6f7ca169b328d9f0394b734afebf78085af4952e
mach594 <mach594@gmail.com>
2025-06-17 16:28:56 -0400

    Create a function to randomize computer game  choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello, World")
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomNumber === 1) {
+        return console.log("scissors")
+    } else if (randomNumber === 2) {
+        return console.log("rock")
+    } else {
+        return console.log("paper")
+    }
+}
+
+
+getComputerChoice()
