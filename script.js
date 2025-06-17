8aca92cacae67dd5bda905e7f905288fe007995e
mach594 <mach594@gmail.com>
2025-06-17 17:37:36 -0400

    Built out script.js
    Added functions to make the game playable. Added playRound and gave results depending on who won.

--- script.js
+++ script.js
@@ -3,13 +3,47 @@ console.log("Hello, World")
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
-        return console.log("scissors")
+        return "scissors"
     } else if (randomNumber === 2) {
-        return console.log("rock")
+        return "rock"
     } else {
-        return console.log("paper")
+        return "paper"
     }
 }
 
+function getHumanChoice() {
+    humanChoice = prompt("Pick rock, paper, scissors: ")
+    return humanChoice.toLowerCase()
+}
 
-getComputerChoice()
+humanScore = 0
+computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            return console.log(`you lose. ${computerChoice} beats ${humanChoice}`)
+        } else {
+            return console.log(`you win. ${humanChoice} beats ${computerChoice}`)
+        } 
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            return console.log(`you lose. ${computerChoice} beats ${humanChoice}`)
+        } else {
+            return console.log(`you win. ${humanChoice} beats ${computerChoice}`)
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            return console.log(`you lose. ${computerChoice} beats ${humanChoice}`)
+        } else {
+            return console.log(`you win. ${humanChoice} beats ${computerChoice}`)
+        }
+    } else {
+        return console.log("it is a tie")
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
